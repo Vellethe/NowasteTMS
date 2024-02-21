971721704ae17e79db88e0e96300716ca81c1939
Vellethe <95373802+Vellethe@users.noreply.github.com>
2024-02-21 12:07:03 +0100

    Controllers

---
+++ NowasteReactTMS.Server/Controllers/OrderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NowasteReactTMS.Server.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ NowasteReactTMS.Server/Controllers/TransportController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NowasteReactTMS.Server.Controllers
+{
+    public class TransportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
