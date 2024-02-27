cd4182649ba9a9ded3d0505695a025f3fdf86d3e
Vellethe <95373802+Vellethe@users.noreply.github.com>
2024-02-27 11:25:20 +0100

    Post controller working

--- NowasteReactTMS.Server/Controllers/OrderController.cs
+++ NowasteReactTMS.Server/Controllers/OrderController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using NowasteReactTMS.Server.Models;
 using NowasteTms.Model;
 using System.Data.SqlTypes;
 using System.Runtime.InteropServices;
@@ -82,12 +83,62 @@ namespace NowasteReactTMS.Server.Controllers
             _inventoryRepository = inventoryRepository;
             _orderLineRepository = orderLineRepository;
         }
+        /// <summary>
+        /// Returns order when searching for its id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpGet]
-        public async Task <IActionResult> GetOrder(Guid id)
+        public async Task <IActionResult> GetOrder(Guid pk)
         {
-             var orders = await _orderRepo.GetOrder(id);
+             var orders = await _orderRepo.GetOrder(pk);
 
             return Ok(orders);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateOrder(OrderDTO dto)
+        {
+            //var user = await _userManager.GetUserAsync(User);
+            Dictionary<Guid, Item> items = new Dictionary<Guid, Item>();
+            foreach (var orderLine in dto.Lines)
+            {
+               var item = await _itemRepository.GetItem(orderLine.ItemPK);
+                items.Add(orderLine.ItemPK, item);
+            }
+            var pk = Guid.NewGuid();
+            var lineNumber = 1;
+            await _orderRepo.AddOrder(new Order
+            {
+                OrderPK = pk,
+                OrderId = dto.OrderId,
+                HandlerID = dto.HandlerID,
+                Type = OrderType.PurchaseOrder,
+                Origin = OrderOrigin.Portal,
+                Status = OrderStatus.Imported,// spo.Status,
+                CollectionDate = dto.CollectionDate,
+                DeliveryDate = dto.DeliveryDate,
+                SupplierPK = dto.SupplierPK,
+                CustomerPK = dto.CustomerPK,
+                Comment = dto.Comment,
+                InternalComment = dto.InternalComment,
+                UpdatedByUserId ="hej",
+                Email ="hej@email.com",
+                TransportBooking = dto.TransportBooking,
+                PalletExchange = dto.PalletExchange,
+                Lines = dto.Lines.Select(x => new OrderLine
+                {
+                    OrderLinePK = Guid.NewGuid(),
+                    OrderPK = pk,
+                    LineNumber = lineNumber++,
+                    ItemPK = items[x.ItemPK].ItemPK,
+                    ItemQty = x.ItemQty,
+                    PalletTypeId = x.PalletTypeId,
+                    PalletQty = x.PalletQty,
+                    ItemName = items[x.ItemPK].Name
+                })
+            });
+            return Ok(pk);
         }
     }
         
---
+++ NowasteReactTMS.Server/Models/OrderDTO.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using NowasteTms.Model;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace NowasteReactTMS.Server.Models
+{
+    public class OrderDTO
+    {
+        [Required]
+        public string OrderId { get; set; }
+        public DateTime CollectionDate { get; set; }
+        public DateTime DeliveryDate { get; set; }
+        public List<OrderLineDTO> Lines { get; set; }
+
+        public bool PalletExchange { get; set; }
+
+        [Required]
+        [DisplayName("Supplier")]
+        public Guid SupplierPK { get; set; }
+
+        [Required]
+        [DisplayName("Customer")]
+        public Guid CustomerPK { get; set; }
+
+        public string HandlerID { get; set; }
+        public string? Comment { get; set; }
+        public string? InternalComment { get; set; }
+        public bool TransportBooking { get; set; }
+    }
+}
