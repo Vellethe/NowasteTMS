bdcd54fe51dcf5df6cf0651260c013c887800983
Vellethe <95373802+Vellethe@users.noreply.github.com>
2024-02-29 12:31:22 +0100

    GetCustomer controller

---
+++ NowasteReactTMS.Server/Controllers/CustomerController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NowasteReactTMS.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : Controller
+    {
+        private readonly ICustomerRepository _customerRepository;
+        private readonly ICurrencyRepository _currencyRepo;
+        public CustomerController(ICustomerRepository customerRepository, ICurrencyRepository currencyRepo)
+        {
+            _customerRepository = customerRepository;
+            _currencyRepo = currencyRepo;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCustomer(Guid id)
+        {
+            var customer = await _customerRepository.GetCustomer(id);
+
+            return Ok(customer);
+        }
+    }
+}
