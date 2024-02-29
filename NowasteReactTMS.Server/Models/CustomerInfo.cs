09493df937f488948471b111d4193d6f4b0d0692
Vellethe <95373802+Vellethe@users.noreply.github.com>
2024-02-29 14:31:21 +0100

    Update CustomerInfo.cs

--- NowasteReactTMS.Server/Models/CustomerInfo.cs
+++ NowasteReactTMS.Server/Models/CustomerInfo.cs
@@ -11,33 +11,31 @@ namespace NowasteReactTMS.Server.Models
         public Guid FinanceInformationPK { get; set; }
         public FinanceInformation FinanceInformation { get; set; }
         public List<ContactInformation> ContactInformations { get; set; }
-        public Guid ReferencePK { get; set; }
-        public Reference Reference { get; set; }
         public string Company { get; set; }
         public bool IsEditable { get; set; }
     }
-    public class CustomerContactInformation
-    {
-        [Key]
-        public Guid ContactInformationPK { get; set; }
-        public Guid BusinessUnitPK { get; set; }
-        public bool IsDefault { get; set; }
-        public Guid? ReferencePK { get; set; }
-        public List<Reference> References { get; set; }
-        public List<TransportZone> TransportZones { get; set; }
-        public string Phone { get; set; }
-        public string CellularPhone { get; set; }
-        public string Email { get; set; }
-        public string Fax { get; set; }
-        public string Address { get; set; }
-        public string Zipcode { get; set; }
-        public string City { get; set; }
-        public string Country { get; set; }
-        public string ExternalId { get; set; }
-        public bool IsActive { get; set; }
-        public string Description { get; set; }
-        public bool IsEditable { get; set; }
-    }
+    //public class CustomerContactInformation
+    //{
+    //    [Key]
+    //    public Guid ContactInformationPK { get; set; }
+    //    public Guid BusinessUnitPK { get; set; }
+    //    public bool IsDefault { get; set; }
+    //    public Guid? ReferencePK { get; set; }
+    //    public List<Reference> References { get; set; }
+    //    public List<TransportZone> TransportZones { get; set; }
+    //    public string Phone { get; set; }
+    //    public string CellularPhone { get; set; }
+    //    public string Email { get; set; }
+    //    public string Fax { get; set; }
+    //    public string Address { get; set; }
+    //    public string Zipcode { get; set; }
+    //    public string City { get; set; }
+    //    public string Country { get; set; }
+    //    public string ExternalId { get; set; }
+    //    public bool IsActive { get; set; }
+    //    public string Description { get; set; }
+    //    public bool IsEditable { get; set; }
+    //}
     public class CustomerFinanceInformation
     {
         public string VAT { get; set; }
