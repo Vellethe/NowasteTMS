3c1291bca179dfb0192eee532cd6fc02ec530cfa
simonhefvelin <113211243+simonhefvelin@users.noreply.github.com>
2024-03-08 13:44:06 +0100

    Set up home page

--- nowastereacttms.client/src/App.jsx
+++ nowastereacttms.client/src/App.jsx
@@ -12,19 +12,22 @@ import Customers from './pages/Transport/Customers';
 import Prices from './pages/Transport/Prices';
 import Suppliers from './pages/Transport/Suppliers';
 import TransportOrderService from './pages/Transport/TransportOrderService';
+import Home from "./pages/Home/Home";
+
 
 
 
 function App() {
-  
- 
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<MasterLayout/>}>
-          <Route path="/" element={<AllOrders />} />         {/* /Transport/Order/AllOrders */}
-
-          <Route path="/Transport/Order/AllHistoricalOrders" element={<AllHistoricalOrders />}
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<MasterLayout />}>
+          <Route path="/Transport/Order/AllOrders" element={<AllOrders />} />{" "}
+          
+          <Route
+            path="/Transport/Order/AllHistoricalOrders"
+            element={<AllHistoricalOrders />}
           />
           <Route path="/Transport/Order/Create" element={<Create />} />
           <Route
@@ -34,15 +37,23 @@ function App() {
           <Route
             path="/Transport/TransportOrder/AllHistoricalTransportOrders"
             element={<AllHistoricalTransportOrders />}
-          />         
-        <Route path="/Transport/TransportZonePrices" element={<Prices/>}/>
-        <Route path="/Transport/Agents/AllAgents" element={<Agents/>}/>
-        <Route path="/Transport/Customers/AllCustomers" element={<Customers/>}/>
-        <Route path="/Transport/Suppliers/AllSuppliers" element={<Suppliers/>}/>
-        <Route path="/Transport/TransportOrderService" element={<TransportOrderService/>}/>
+          />
+          <Route path="/Transport/TransportZonePrices" element={<Prices />} />
+          <Route path="/Transport/Agents/AllAgents" element={<Agents />} />
+          <Route
+            path="/Transport/Customers/AllCustomers"
+            element={<Customers />}
+          />
+          <Route
+            path="/Transport/Suppliers/AllSuppliers"
+            element={<Suppliers />}
+          />
+          <Route
+            path="/Transport/TransportOrderService"
+            element={<TransportOrderService />}
+          />
         </Route>
-        <Route path="/Identity/Account/Login" element={<Login/>} />
-
+        <Route path="/Identity/Account/Login" element={<Login />} />
       </Routes>
     </Router>
   );
