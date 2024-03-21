5e3030db1dd4ac548dc710df2f9c2c76d17f9fdf
Vellethe <95373802+Vellethe@users.noreply.github.com>
2024-03-21 11:05:17 +0100

    DateTime fixes

--- nowastereacttms.client/src/components/APICalls/Prices/GetAllPrices.js
+++ nowastereacttms.client/src/components/APICalls/Prices/GetAllPrices.js
@@ -14,10 +14,28 @@ const getAllPrices = async() => {
         throw new Error('Failed to fetch prices');
       }
       const data = await response.json();
+
+      data.forEach(item => {
+        item.price.validFrom = formatDatetime(item.price.validFrom);
+        item.price.validTo = formatDatetime(item.price.validTo);
+      });
+
       return data;
     } catch (error) {
       throw new Error('Error fetching prices: ' + error.message);
     }
   };
+
+  function formatDatetime(datetimeString) {
+    const datetime = new Date(datetimeString);
+    const day = datetime.getDate().toString().padStart(2, '0');
+    const month = (datetime.getMonth() + 1).toString().padStart(2, '0');
+    const year = datetime.getFullYear();
+    const hours = datetime.getHours().toString().padStart(2, '0'); 
+    const minutes = datetime.getMinutes().toString().padStart(2, '0');
+    const seconds = datetime.getSeconds().toString().padStart(2, '0');
+    const dayOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][datetime.getDay()];
+    return `${dayOfWeek}, ${day}-${month}-${year} ${hours}:${minutes}:${seconds}`;
+  }
   
   export default getAllPrices;
