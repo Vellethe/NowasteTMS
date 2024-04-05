b331a0dc9f5de38cdf66fcd19c9eced79bf9070c
simonhefvelin <113211243+simonhefvelin@users.noreply.github.com>
2024-04-05 13:48:40 +0200

    Fixing a mistake

--- nowastereacttms.client/src/components/Footer.jsx
+++ nowastereacttms.client/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="flex bg-brown w-full fixed bottom-0 left-0 h-x p-6 text-white justify-center mt-5">
+    <footer className="flex bg-brown w-full bottom-0 left-0 h-x p-6 text-white justify-center mt-5">
       <section className="flex flex-col gap-3 ">
         <span className="text-3xl">Nowaste Logistics</span>
         <span className="text-sm text-center">Landskronavägen 5b | Helsingborg </span>
