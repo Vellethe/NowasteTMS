14a993f52452da0ba27eabd75c3966af17ac5e14
simonhefvelin <113211243+simonhefvelin@users.noreply.github.com>
2024-04-05 08:44:19 +0200

    All that is needed for log in I think

---
+++ NowasteReactTMS.Server/Models/Identity/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NowastePalletPortal
+{
+    public static class Constants
+    {
+        public const string ROLE_ADMIN = "Admin";
+        public const string ROLE_SUPERUSER = "Superuser";
+        public const string ROLE_STANDARDUSER = "Standarduser";
+        public const string ROLE_AGENT = "Agent";
+    }
+}
---
+++ NowasteReactTMS.Server/Repositories/UserAccountRepository.cs
@@ -0,0 +1,87 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Identity;
+using NowastePalletPortal.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Transactions;
+using WMan.Data.ConnectionFactory;
+
+namespace NowastePalletPortal.Extensions.Helpers
+{
+    public class UserAccountRepository : IUserAccountRepository
+    {
+        private readonly IConnectionFactory connectionFactory;
+
+        public UserAccountRepository(IConnectionFactory connectionFactory)
+        {
+            this.connectionFactory = connectionFactory;
+        }
+
+        public async Task UpdateUser(ApplicationUser user)
+        {
+            using (var connection = connectionFactory.CreateConnection())
+            {
+                const string sqlUpdate = @"UPDATE [dbo].[AspNetUsers]
+                               SET [PhoneNumber] = @PhoneNumber
+                                  ,[BusinessUnitId] = @BusinessUnitId
+                                  ,[PasswordHash] = @PasswordHash
+                                  ,[DivisionId] = @DivisionId
+                               WHERE [Id] = @Id;";
+                await connection.ExecuteAsync(sqlUpdate, new
+                {
+                    user.PhoneNumber,
+                    user.BusinessUnitId,
+                    user.PasswordHash,
+                    user.DivisionId,
+                    user.Id
+                });
+            }
+        }
+
+        public async Task SetUserPalletAccount(string userId, long palletAccountId)
+        {
+            using (var connection = connectionFactory.CreateConnection())
+            {
+                const string sqlInsert = @"UPDATE [dbo].[AspNetUsers]
+                               SET [PalletAccountId] = @PalletAccountId
+                               WHERE [Id] = @UserId;";
+                await connection.ExecuteAsync(sqlInsert, new { UserId = userId, PalletAccountId = palletAccountId });
+            }
+        }
+
+        public async Task SetUserRole(string userId, List<string> rolesForUser)
+        {
+            using (var scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
+            using (var connection = connectionFactory.CreateConnection())
+            {
+                // Delete old values
+                const string sqlDelete = @"DELETE FROM [dbo].[AspNetUserRoles] WHERE [UserId] = @userId";
+                await connection.QueryAsync<IdentityUserRole<string>>(sqlDelete, new { UserId = userId });
+
+                foreach (var roleId in rolesForUser)
+                {
+                    // Insert new values
+                    const string sqlInsert = @"INSERT INTO [dbo].[AspNetUserRoles]
+                                ([UserId]
+                                 ,[RoleId])
+                    VALUES
+                    (@UserId, @RoleId);";
+                    await connection.ExecuteAsync(sqlInsert, new { UserId = userId, RoleId = roleId });
+                }
+
+                scope.Complete();
+            }
+        }
+
+        public async Task<IEnumerable<Division>> GetDivisions()
+        {
+            using (var connection = connectionFactory.CreateConnection())
+            {
+                const string sql = @"SELECT [Id]
+                                           ,[Name]
+                                        FROM [dbo].[Division];";
+                return await connection.QueryAsync<Division>(sql);
+            }
+        }
+    }
+}
